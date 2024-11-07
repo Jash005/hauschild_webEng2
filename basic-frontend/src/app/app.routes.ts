5eeae80a48871b21f67d0da6b5f77c387b486ddc
Jasminh <mail@jasminh.de>
2024-11-07 16:27:50 +0100

    add login and register

--- basic-frontend/src/app/app.routes.ts
+++ basic-frontend/src/app/app.routes.ts
@@ -1,11 +1,23 @@
 import {Routes} from '@angular/router';
 import {WelcomeComponent} from './features/welcome/welcome.component';
 import {PageNotFoundComponent} from './features/page-not-found/page-not-found.component';
+//NOTE - Hier Import von neuen Routen hinzufügen
+import {RegisterComponent} from './features/register/register.component';
+import {LoginComponent} from './features/login/login.component';
 
 export const routes: Routes = [
   {
     path: '',
     component: WelcomeComponent
+  },
+  //NOTE - Hier neue Routen hinzufügen
+  {
+    path: 'register',
+    component: RegisterComponent
+  },
+  {
+    path: 'login',
+    component: LoginComponent
   },
   {
     path: '404',
---
+++ basic-frontend/src/app/features/register/register.component.ts
@@ -0,0 +1,30 @@
+import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-register',
+  standalone: true,
+  imports: [
+    ReactiveFormsModule
+  ],
+  templateUrl: './register.component.html',
+  styleUrl: './register.component.css'
+})
+export class RegisterComponent {
+  registerForm: FormGroup;
+
+  constructor(private fb: FormBuilder) {
+    this.registerForm = this.fb.group({
+      username: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(6)]],
+      acceptTerms: [false, Validators.requiredTrue]
+    });
+  }
+
+  submitForm() {
+    if (this.registerForm.valid) {
+      console.log('Form Submitted', this.registerForm.value);
+    }
+  }
+}
