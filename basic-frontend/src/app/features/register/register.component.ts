007be437013166b60a79c8112a058ead2e2f3b3d
Jasminh <mail@jasminh.de>
2024-11-07 16:43:52 +0100

    add DesignLibary to register

--- basic-frontend/src/app/features/register/register.component.ts
+++ basic-frontend/src/app/features/register/register.component.ts
@@ -1,11 +1,20 @@
 import { Component } from '@angular/core';
-import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
   standalone: true,
   imports: [
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    MatInputModule,
+    MatButtonModule,
+    MatCheckboxModule,
+    MatFormFieldModule
   ],
   templateUrl: './register.component.html',
   styleUrl: './register.component.css'
